2221fc1c5f9bc15a3f65ea956634b5b61f4da9ce
pawanhirumina <pawanhirumina71@gmail.com>
2025-05-13 23:37:13 +0530

    Add custom 404 page with routing fallback for unknown paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import MovieSearch from "./components/MovieSearch";
 import Footer from "./components/Footer";
+import NotFound from "./components/NotFound";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div style={{ flex: 1 }}>
-      <MovieSearch />
-      <Footer />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MovieSearch />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+    </BrowserRouter>
   );
 }
 
